bcccf711cdafdc4f4e0ba9033ab3a1c63d90a137
cheshire <garfield@snu.ac.kr>
2023-11-19 15:59:38 +0900

    주석 추가

--- src/main/kotlin/com/example/webfluxdemo/config/RequestHandler.kt
+++ src/main/kotlin/com/example/webfluxdemo/config/RequestHandler.kt
@@ -12,6 +12,10 @@ import reactor.core.publisher.Mono
 
 @Service
 class RequestHandler(val mathService: ReactiveMathService) {
+    // Handler Function들의 집합
+    // handle() 메소드 하나만 노출하면 되므로 아래 메소드들이 곧 handler function이 된다.
+    // ServerRequest를 받아서 ServerResponse를 반환하면 된다.
+    // 이 때 ServerResponse는 ok() 등의 Builder를 통해 생성하는 것이 일반적이다.
     fun squareHandler(request: ServerRequest): Mono<ServerResponse> {
         val input = request.pathVariable("input").toInt()
         val responseMono = mathService.findSquare(input)
@@ -28,19 +32,19 @@ class RequestHandler(val mathService: ReactiveMathService) {
         val input = request.pathVariable("input").toInt()
         val responseFlux = mathService.multiplicationTable(input)
         return ServerResponse.ok()
-                .contentType(MediaType.TEXT_EVENT_STREAM)
-                .body(responseFlux, Response::class.java)
+            .contentType(MediaType.TEXT_EVENT_STREAM)
+            .body(responseFlux, Response::class.java)
     }
 
     fun multiplyHandler(request: ServerRequest): Mono<ServerResponse> {
         val requestDto = request.bodyToMono(MultiplyRequestDto::class.java)
         val responseMono = this.mathService.multiply(requestDto)
         return ServerResponse.ok()
-                .body(responseMono, Response::class.java)
+            .body(responseMono, Response::class.java)
     }
 
     fun squareHandlerWithValidation(request: ServerRequest): Mono<ServerResponse> {
-        val input = Integer.parseInt(request.pathVariable("input"));
+        val input = Integer.parseInt(request.pathVariable("input"))
         if (input < 10 || input > 20) {
             return Mono.error(InputFailedException(input))
         }
--- src/main/kotlin/com/example/webfluxdemo/config/RouterConfig.kt
+++ src/main/kotlin/com/example/webfluxdemo/config/RouterConfig.kt
@@ -12,30 +12,29 @@ class RouterConfig(val requestHandler: RequestHandler) {
     @Bean
     fun routerFunctions(): RouterFunction<ServerResponse> {
         return RouterFunctions.route()
-                .path("router", ::serverResponseRouterFunction)
-                .build()
+            .path("router", ::serverResponseRouterFunction)
+            .build()
     }
-
 
     private fun serverResponseRouterFunction(): RouterFunction<ServerResponse> {
         return RouterFunctions.route()
-                    .GET("square/{input}", RequestPredicates.path("*/1?") , requestHandler::squareHandler)
-                    .GET("square/{input}") {
-                        ServerResponse.badRequest().bodyValue("Only 10-19 allowed")
-                    }
-                    .GET("table/{input}", requestHandler:: tableHandler)
-                    .GET("table/{input}/stream", requestHandler:: tableStreamHandler)
-                    .POST("multiply", requestHandler:: multiplyHandler)
-                    .GET("square/{input}/validation", requestHandler::squareHandlerWithValidation)
-                .onError(InputFailedException::class.java, exceptionHandler())
-                 .build()
-
+            // GET(pattern, predicate, HandlerFunction)
+            .GET("square/{input}", RequestPredicates.path("*/1?"), requestHandler::squareHandler)
+            .GET("square/{input}") {
+                ServerResponse.badRequest().bodyValue("Only 10-19 allowed")
+            }
+            .GET("table/{input}", requestHandler::tableHandler)
+            .GET("table/{input}/stream", requestHandler::tableStreamHandler)
+            .POST("multiply", requestHandler::multiplyHandler)
+            .GET("square/{input}/validation", requestHandler::squareHandlerWithValidation)
+            .onError(InputFailedException::class.java, exceptionHandler())
+            .build()
     }
 }
 
 fun handle(err: Throwable, req: ServerRequest): Mono<ServerResponse> {
     val ex = err as InputFailedException
-    val response = InputFailedException(input=ex.input, message = ex.message, errorCode = ex.errorCode)
+    val response = InputFailedException(input = ex.input, message = ex.message, errorCode = ex.errorCode)
     return ServerResponse.badRequest().bodyValue(response)
 }
 private fun exceptionHandler(): BiFunction<Throwable, ServerRequest, Mono<ServerResponse>> = BiFunction(::handle)
